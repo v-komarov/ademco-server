3e7ba73477fbe066c4d46dd588f3e4769045a22b
vladimir komarov <komarov-krsk@mail.ru>
2016-08-16 14:52:09 +0700

    Вариант контроля зомби соединений.

--- ademco.erl
+++ ademco.erl
@@ -1,13 +1,23 @@
 -module(ademco).
--export([start/0,server/0,wait_connect/1]).
+-export([start/0,server/0,wait_connect/1,dog/1,check_proc/1]).
 
 
--define(TCP_ACCEPT_TIMEOUT, 1000).
--define(TCP_RECV_TIMEOUT, 1000).
+-define(DEBUG,true).
+
+-ifdef(DEBUG).
 -define(CMD_OFFICER,"cd ~/django/sur;python manage.py ademco-officer ").
 -define(CMD_OFFICER_SYNC,"cd ~/django/sur;python manage.py officer-sync ").
 -define(CMD_GENPW,"cd ~/django/sur;python manage.py gen-pw ").
 -define(CMD_CHECKDES,"cd ~/django/sur;python manage.py check-des ").
+-else.
+-define(CMD_OFFICER,"cd /srv/django/sur;python manage.py ademco-officer ").
+-define(CMD_OFFICER_SYNC,"cd /srv/django/sur;python manage.py officer-sync ").
+-define(CMD_GENPW,"cd /srv/django/sur;python manage.py gen-pw ").
+-define(CMD_CHECKDES,"cd /srv/django/sur;python manage.py check-des ").
+-endif.
+
+-define(TCP_ACCEPT_TIMEOUT, 1000).
+-define(TCP_RECV_TIMEOUT, 1000).
 
 
 %% Сервер приема сообщений от оборудования протокол ademco
@@ -15,10 +25,18 @@
 
 
 start() ->
+
+       Tabid = ets:new(aliveTab,[ordered_set,public]),
+
        process_flag(trap_exit, true),
        Pid = spawn_link(?MODULE, server, []),
-       io:format("server has been started ~p~n",[calendar:local_time()]),
-	   loop(Pid).
+       register(main,Pid),
+       io:format("server has been started ~p ~w~n",[calendar:local_time(),Pid]),
+       Pid2 = spawn_link(?MODULE, dog, [Tabid]),
+       register(dw,Pid2),
+       io:format("dog has been started ~p ~w~n",[calendar:local_time(),Pid2]),
+       loop(Pid).
+
 
 
 
@@ -33,9 +51,48 @@ loop(Pid) ->
 
 
 
+
+
+
+%% Наблюдение за состоянием связи
+dog(Tabid) ->
+    receive
+        {alive,Pid} ->
+            ets:insert(Tabid,{Pid,erlang:timestamp()}),
+            io:format("~w~n",[ets:match_object(Tabid,{'_','_'})]),
+            io:format("alive ~w~n",[Pid]),
+            check_proc(Tabid);
+        _Other -> dog(Tabid)
+    end,
+    dog(Tabid).
+
+
+
+
+
+check_proc(Tabid) ->
+
+    lists:map(
+       fun(Elem) ->
+          io:format("~w~n",[timer:now_diff(erlang:timestamp(),element(2,Elem))/1000]),
+          A = timer:now_diff(erlang:timestamp(),element(2,Elem)) / 1000,
+          if A  > 86400 ->
+            ets:match_delete(Tabid,{element(1,Elem),'_'}),
+            exit(element(1,Elem),timeout);
+            true -> ok
+          end
+       end, ets:match_object(Tabid,{'_','_'})  ),
+
+    dog(Tabid).
+
+
+
+
+
 server() ->
     {ok, ListenSocket} = gen_tcp:listen(11112, [binary, {active, false}]),
     wait_connect(ListenSocket).
+
 
 
 
@@ -49,15 +106,16 @@ wait_connect(ListenSocket) ->
 
 
 
+
 %%% Чтение первого байта после соединения
 get_request(Socket, BinaryList) ->
     case gen_tcp:recv(Socket, 1) of
 	{ok, Binary} ->
 
-	    io:format("Connect ~p~n",[self()]),
+	    io:format("Connect ~w~n",[self()]),
 	    case Binary of
             <<249>> -> genword(Socket);
-            <<_>> -> io:format("error ~p~p~n",[calendar:local_time(),Binary]);
+            <<_>> -> io:format("error ~p~p ~w~n",[calendar:local_time(),Binary,self()]);
             true -> ok
 
         end,
@@ -78,13 +136,15 @@ sync_answer(Socket) ->
             <<Panel:4/binary,Last:1/binary>> = Binary,
 
             if Last == <<251>> ->
-                io:format("sync ~p ~p~n",[calendar:local_time(),Panel]),
+                io:format("sync ~p ~p ~w~n",[calendar:local_time(),Panel,self()]),
 
                 Data = binary_to_list(Panel),
                 Cmd = lists:concat([?CMD_OFFICER_SYNC,Data]),
                 os:cmd(Cmd),
 
-                io:format("~p ~p~n",[calendar:local_time(),Cmd]),
+                io:format("~p ~p ~w~n",[calendar:local_time(),Cmd,self()]),
+
+                dw ! {alive,self()},
 
                 Ans = list_to_binary([<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>]),
                 gen_tcp:send(Socket,<<Ans/binary>>);
@@ -109,32 +169,32 @@ genword(Socket) ->
                 Ver = binary_to_list(Version),
                 Cmd = lists:concat([?CMD_GENPW,Id_panel," ",Ver]),
                 A = element(1, lists:split(8,os:cmd(Cmd)) ),
-                io:format("~p ~p ~p~n",[calendar:local_time(),Cmd,A]),
+                io:format("~p ~p ~p ~w~n",[calendar:local_time(),Cmd,A,self()]),
                 B = list_to_binary(A),
                 Ans = <<255,B/binary,255,26>>,
-                io:format("~p ~n",[Ans]),
+                io:format("~p ~w ~n",[Ans,self()]),
                 gen_tcp:send(Socket,<<Ans/binary>>),
 
                     case gen_tcp:recv(Socket,18) of
                         {ok, Bin} ->
                         <<D:17/binary,250>> = Bin,
                         <<250,DES:16/binary>> = D,
-                        io:format("DES ~p~n",[binary_to_list(DES)]),
+                        io:format("DES ~p ~w~n",[binary_to_list(DES),self()]),
                         Cmd2 = lists:concat([?CMD_CHECKDES,Id_panel," ",binary_to_list(DES)]),
-                        io:format("~p ~p~n",[calendar:local_time(),Cmd2]),
+                        io:format("~p ~p ~w~n",[calendar:local_time(),Cmd2,self()]),
                         K = os:cmd(Cmd2),
-                        io:format("CHECK ~p~n",[K]),
+                        io:format("CHECK ~p ~w~n",[K,self()]),
 
 
                         if
                            K == "True\n" ->
                            Ans2 = list_to_binary([<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>]),
                            gen_tcp:send(Socket,<<Ans2/binary>>),
-                           io:format("CONNECT OK ~n",[]),
+                           io:format("CONNECT OK ~w~n",[self()]),
                            get_message(Socket);
 
                            K == "False\n" ->
-                           io:format("CONNECT ERROR ~n",[]),
+                           io:format("CONNECT ERROR ~w~n",[self()]),
                            get_request(Socket,[])
 
                         end
@@ -159,7 +219,7 @@ get_message(Socket) ->
 	        <<255>> -> get_ademco(Socket);
             <<254>> -> get_officer(Socket,[]);
             <<251>> -> sync_answer(Socket);
-            <<_>> -> io:format("error ~p~p~n",[calendar:local_time(),Binary]);
+            <<_>> -> io:format("error ~p~p ~w~n",[calendar:local_time(),Binary,self()]);
             true -> ok
 
         end,
@@ -183,9 +243,9 @@ get_ademco(Socket) ->
 
             Cmd = lists:concat([?CMD_OFFICER,Data]),
             os:cmd(Cmd),
-            io:format("~p ~p~n",[calendar:local_time(),Data]),
+            io:format("~p ~p ~w~n",[calendar:local_time(),Data,self()]),
             Ans = list_to_binary([<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>]),
-            io:format("~p ~p~n",[calendar:local_time(),Cmd]),
+            io:format("~p ~p ~w~n",[calendar:local_time(),Cmd,self()]),
             gen_tcp:send(Socket,<<Ans/binary>>),
             get_message(Socket)
 
@@ -204,7 +264,7 @@ get_officer(Socket,BinaryList) ->
         if Binary == <<254>> ->
             Data = list_to_binary(lists:reverse(BinaryList)),
             DataList = binary_to_list(Data),
-	        io:format("~p ~p~n",[calendar:local_time(),DataList]),
+	        io:format("~p ~p ~w~n",[calendar:local_time(),DataList,self()]),
             Ans = list_to_binary([<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>,<<26>>]),
             gen_tcp:send(Socket,<<Ans/binary>>),
             get_request(Socket,[]);
